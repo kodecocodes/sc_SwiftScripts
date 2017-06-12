0f578821f1263dfa2d98ac209760dfacf7730911
Sam Davies <sam@razeware.com>
2017-06-12 18:10:21 +0100

    Added primitive argument parsing

--- PasswordGenerator.swift
+++ PasswordGenerator.swift
@@ -77,10 +77,30 @@ struct SafariPasswordGenerator: RandomStringGenerator {
   }
 }
 
+var count: Int = 1
+var length: Int = 15
+var safariGenerator = false
 
-let generator = SafariPasswordGenerator(count: 5)
+for argument in CommandLine.arguments {
+  if argument.starts(with: "--length=") {
+    length = Int(argument.substring(from: "--length=".endIndex)) ?? length
+  } else if argument.starts(with: "--count=") {
+    count = Int(argument.substring(from: "--count=".endIndex)) ?? count
+  } else if argument == "--safari" {
+    safariGenerator = true
+  }
+}
+
+let generator: RandomStringGenerator
+
+if safariGenerator {
+  generator = SafariPasswordGenerator(count: count)
+} else {
+  generator = PseudoRandomPasswordGenerator(length: length, count: count)
+}
+
 let passwords = generator.generate()
 
-dump(CommandLine.arguments)
-
 print(passwords.joined(separator: "\n"))
+
+
